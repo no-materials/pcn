c470eb294223271cbaa31be0eff5a1e9fcc78bd5
nomaterials <nomaterials@gmail.com>
2020-01-18 14:08:36 +0200

    debug

--- test_shapenet.py
+++ test_shapenet.py
@@ -23,7 +23,7 @@ def test(args):
 
     output = tf.placeholder(tf.float32, (1, 16384, 3))
     cd_op = chamfer(output, gt)
-    emd_op = earth_mover(output, gt)
+    # emd_op = earth_mover(model.coarse, gt[:, :model.coarse.shape[1], :])
 
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
@@ -52,8 +52,9 @@ def test(args):
         start = time.time()
         completion = sess.run(model.outputs, feed_dict={inputs: [partial], npts: [partial.shape[0]]})
         total_time += time.time() - start
-        cd, emd = sess.run([cd_op, emd_op], feed_dict={output: completion, gt: [complete]})
+        cd = sess.run([cd_op], feed_dict={output: completion, gt: [complete]})
         total_cd += cd[0]
+        emd = 0
         total_emd += emd
         writer.writerow([model_id, cd, emd])
 
